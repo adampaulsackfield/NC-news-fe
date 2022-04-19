f4c95e19b778083660964732633aba90f1ee12c3
Adam Sackfield <adamsackfield20@gmail.com>
2022-04-19 15:59:41 +0100

    implement react-router-dom, basic app structutre setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Users from './components/Users';
+import Articles from './components/Articles';
+
 const App = () => {
 	return (
 		<div className='bg-dark text-white'>
-			<h1>App Component</h1>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/users' element={<Users />} />
+				<Route path='/articles' element={<Articles />} />
+			</Routes>
 		</div>
 	);
 };
