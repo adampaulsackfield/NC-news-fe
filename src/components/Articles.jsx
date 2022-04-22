f80d46a7785e33e9fc3af6423606a28996cbe8e0
Adam Sackfield <adamsackfield20@gmail.com>
2022-04-22 17:16:32 +0100

    remoed unused imports

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 // TODO - Links for username, title and buttons for removing and viewing
 
-import { getArticles, getTopics } from '../actions/api';
+import { getArticles } from '../actions/api';
 import ArticleCard from './ArticleCard';
 import TopicFilter from './TopicFilter';
 
